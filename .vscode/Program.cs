4f7f9ba08bcd170597157523bb186cfbbeb68f4f
mehrdadhafezi <mhafezi98@gmail.com>
2022-03-11 14:42:43 +0330

    e4

---
+++ .vscode/Program.cs
@@ -0,0 +1,23 @@
+﻿int date;
+System.Console.WriteLine("give me the month");
+date=Convert.ToInt32(Console.ReadLine());
+
+
+switch (date)
+{
+    case 1:
+    System.Console.WriteLine("Janury");
+        break;
+    case 2:
+    System.Console.WriteLine("February");
+    break;
+    case 3:
+    System.Console.WriteLine("March");
+    break;
+    case 4:
+    System.Console.WriteLine("April");
+    break;
+    default:
+    System.Console.WriteLine("nadarim oskol");
+        break;
+}
--- Program.cs
+++ Program.cs
@@ -1,2 +1,25 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿int a;
+for (var i = 0; i < 4; i++)
+{
+System.Console.WriteLine("wage");
+a=Convert.ToInt32(Console.ReadLine());
+if (a>2000000 && a<3000000)
+{
+    System.Console.WriteLine(a*0.05);
+}
+
+if (a>3000000)
+{
+    System.Console.WriteLine(a*0.07);
+}
+
+if (a<2000000)
+{
+    System.Console.WriteLine(a*0);
+}
+
+
+
+
+
+}
